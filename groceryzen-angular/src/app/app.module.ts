3be4ea3dc77d532acc19113573bedc947115697c
Michael Rosario <michael.p.rosario@gmail.com>
2019-03-24 23:58:48 -0400

    feat ( shopping lists ) Implement material list

--- groceryzen-angular/src/app/app.module.ts
+++ groceryzen-angular/src/app/app.module.ts
@@ -17,7 +17,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -29,6 +31,8 @@ import {MatCardModule} from '@angular/material/card';
     RegisterComponent
   ],
   imports: [
+    MatToolbarModule,
+    MatListModule,
     MatCardModule,
     MatButtonModule,
     MatFormFieldModule,
